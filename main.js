4b540e0f954fe97f0d15cbfab9a65edd96b9213e
Goluraja-hue <kumargolu934113@gmail.com>
2025-09-19 18:13:37 +0530

    questionns

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-for (i=1;i<=10;i++){
-    for (j=1;j<=10;j++){
-        console.log(`${i}x${j}=${i*j}`);
-    }
-    console.log("-----");
-}
+// for (i=1;i<=10;i++){
+//     for (j=1;j<=10;j++){
+//         console.log(`${i}x${j}=${i*j}`);
+//     }
+//     console.log("-----");
+// }/
+
+
+// let arr=[10,12,3,4,15,16,17];
+// let index = arr.findIndex((e)=> e === 3
+// );
+// console.log(index);
+// arr [index] = 22 ;
+// console.log(arr)
+
+
+arr2 = [2, 34, 4, 56, 6, 6, 6, 8];
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
+// Q 1. interchange their elements of position like all even positions ? like, and positions = are , 2,4,6,8
+let idx = arr1.findIndex((e)=>{
+    e === 2;
+});
+arr1 [idx] = 30;
+console.log(idx);
+console.log(arr1);
+
+
+
+
+
+// Q 2. create a function which will take  this array as parameter and and element and returns elements position?
+// Q3. Now create a function which will take and element as params and change its value to another number from params in arr2.
+
+// Q4. a function which takes an element as params and checks if it is in arr1 or not .
+// Q5. create a function which will take an array as params and returns the sum of all elements of that array.
